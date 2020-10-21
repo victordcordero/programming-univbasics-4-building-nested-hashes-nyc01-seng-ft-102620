ea51bbd6fc58d9e42c0c93bc086efeb6e74ebcd7
Victor Cordero <victordcordero@gmail.com>
2020-10-21 20:31:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -43,7 +43,8 @@ def monopoly_with_fourth_tier
     :rent_in_dollars => {
       :one_piece_owned => 25,
       :two_pieces_owned => 50,
-      :three_pieces_owned => 200,
+      :three_pieces_owned => 100,
+      :four_pieces_owned => 200,
     },
     :names => {
       :reading_railroad => [mortgage_value: 100],
