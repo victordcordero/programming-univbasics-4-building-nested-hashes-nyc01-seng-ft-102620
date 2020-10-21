53c4e8dccda1b5f5b24029756ab10af637ff50b6
Victor Cordero <victordcordero@gmail.com>
2020-10-21 20:25:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -22,7 +22,11 @@ end
 def monopoly_with_third_tier
   {:railroads => {
     :pieces => 4,
-    :rent_in_dollars => {},
+    :rent_in_dollars => {
+      :one_piece_owned => 25,
+      :two_pieces_owned => 50,
+      :three_pieces_owned => 200,
+    },
     :names => {},
     }
   }
